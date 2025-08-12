a0e53a57dd3dc3c6478861f6610233507030750b
robin <comfysage@isabelroses.com>
2025-08-12 22:53:55 +0200

    keymaps: add `<c-j>` keymap

--- lua/sylvee/keymaps.lua
+++ lua/sylvee/keymaps.lua
@@ -4,7 +4,7 @@ vim.keymap.set("n", "[<tab>", vim.cmd.tabprev)
 vim.keymap.set("n", "]<tab>", vim.cmd.tabnext)
 vim.keymap.set("n", "<C-w><tab>", vim.cmd.tabnew)
 
-vim.keymap.set("n", "<leader>G", "<cmd>FloatermNew --autoclose=1 lazygit<CR>")
+vim.keymap.set("n", "<C-j>", "<C-^>")
 
 vim.keymap.set("n", "<leader>i", vim.cmd.Inspect)
 vim.keymap.set("n", "<leader>I", vim.cmd.InspectTree)
